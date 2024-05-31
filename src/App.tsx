f4e595a44d9c5bd15383099a019e8f3d1ebc1fcf
Hubert Ryan <hubertryanofficial@gmail.com>
2024-05-31 12:06:33 -0300

    feat: new get in touch button

--- src/App.tsx
+++ src/App.tsx
@@ -6,48 +6,52 @@ import ExternalLink from "./components/ExternalLink";
 import HeaderInfo from "./components/HeaderInfo";
 
 import TabsInfo from "./components/tabs";
+import { Toaster } from "./components/ui/toaster";
 
 function App() {
   return (
-    <div className="w-full min-h-[100vh] flex justify-center bg-white overflow-y-auto">
-      <main className="max-w-[750px] h-[750px] mt-16 px-8">
-        <header>
-          <div className="flex flex-row items-end justify-between">
-            <motion.img
-              initial={{
-                opacity: 0,
-              }}
-              animate={{ opacity: 1 }}
-              whileHover={{
-                scale: 1.1,
-              }}
-              transition={{
-                duration: 0.4,
-              }}
-              className="w-32 h-w-32 rounded-2xl sm:w-40 sm:h-40"
-              src="https://avatars.githubusercontent.com/u/51359763?v=4"
-            />
-            <div className="flex flex-col items-end gap-y-2 sm:flex-row sm:gap-x-4">
-              <ExternalLink
-                title="Github"
-                link="https://github.com/hubertryanofficial"
+    <>
+      <div className="w-full min-h-[100vh] flex justify-center bg-white overflow-y-auto">
+        <main className="max-w-[800px] h-[750px] mt-16 px-8">
+          <header>
+            <div className="flex flex-row items-end justify-between">
+              <motion.img
+                initial={{
+                  opacity: 0,
+                }}
+                animate={{ opacity: 1 }}
+                whileHover={{
+                  scale: 1.1,
+                }}
+                transition={{
+                  duration: 0.4,
+                }}
+                className="w-32 h-w-32 rounded-2xl sm:w-40 sm:h-40"
+                src="https://avatars.githubusercontent.com/u/51359763?v=4"
               />
-              <ExternalLink
-                title="X @hubertryanoff"
-                link="https://x.com/hubertryanoff"
-              />
-              <ExternalLink
-                title="Instagram"
-                link="https://instagram.com/hubertryanofficial"
-              />
+              <div className="flex flex-col items-end gap-y-2 sm:flex-row sm:gap-x-4">
+                <ExternalLink
+                  title="Github"
+                  link="https://github.com/hubertryanofficial"
+                />
+                <ExternalLink
+                  title="X @hubertryanoff"
+                  link="https://x.com/hubertryanoff"
+                />
+                <ExternalLink
+                  title="Instagram"
+                  link="https://instagram.com/hubertryanofficial"
+                />
+              </div>
             </div>
-          </div>
-          <HeaderInfo />
-        </header>
-        <Separator className="my-6" />
-        <TabsInfo />
-      </main>
-    </div>
+            <HeaderInfo />
+          </header>
+          <Separator className="my-6" />
+          <TabsInfo />
+        </main>
+      </div>
+      <Toaster />
+    </>
   );
 }
 
--- src/components/HeaderInfo.tsx
+++ src/components/HeaderInfo.tsx
@@ -1,8 +1,13 @@
 import { memo } from "react";
 
 import { motion } from "framer-motion";
+import { Button } from "./ui/button";
+import { Copy, Send } from "lucide-react";
+import { useToast } from "./ui/use-toast";
 
 const HeaderInfo = memo(() => {
+  const { toast } = useToast();
+
   return (
     <div className="mt-6">
       <h1 className="text-base font-medium sm:text-lg">@hubertryan</h1>
@@ -12,11 +17,33 @@ const HeaderInfo = memo(() => {
         transition={{
           duration: 0.4,
         }}
-        className="mt-4 text-2xl text-gray-400 sm:text-3xl"
+        className="mt-4 text-2xl text-black sm:text-3xl"
       >
         Hi, I'm a fullstack developer, UI/UX designer and musician from Brazil.
         🇧🇷
       </motion.p>
+      <div className="mt-2 flex flex-row justify-end gap-x-2">
+        <Button
+          onClick={() =>
+            window.location.assign("mailto:hubertryanofficial@gmail.com")
+          }
+        >
+          <Send className="mr-2 w-[20px]" /> Get in touch
+        </Button>
+        <Button
+          variant="ghost"
+          onClick={() => {
+            navigator.clipboard.writeText("contact@sixcry.com");
+            toast({
+              title: "E-mail address copied.",
+              description: "Waiting for you contact.",
+              variant: "default",
+            });
+          }}
+        >
+          <Copy />
+        </Button>
+      </div>
     </div>
   );
 });
