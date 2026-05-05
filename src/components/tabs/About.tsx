ce051229b8125b8a23fd369d142e41bcc30e33af
Hubert Ryan <hubertryanofficial@gmail.com>
2026-05-05 03:25:02 -0300

    fix: about me

--- src/components/tabs/About.tsx
+++ src/components/tabs/About.tsx
@@ -25,38 +25,11 @@ export default function About() {
       <p>
         Apart from my programming career, I am a self-taught person passionate
         about mathematics, philanthropy and things that happen in the world. If
-        you want to know more, follow me on social media.
-      </p>
-      <h2 className="text-xl italic my-6 font-semibold">
-        Why I founded Tapedin, Cloudsky and Sixcry
-      </h2>
-      <p className="text-base text-justify">
-        <b>Why Tapedin?</b> I founded it with the aim of improving the
-        experience by sharing everyday moments, the main motivation for founding{" "}
-        <b>Tapedin</b> is to break the parallel reality among social media users
-        today. I believe that with Tapedin I can change the way people
-        communicate around the world by sharing real moments and reactions
-        rather than just clicks.
-      </p>
-      <br />
-      <p className="text-bas  text-justify">
-        <b>Why Cloudsky?</b> Seeing the absurd growth of Internet users with the
-        responsibility of having several passwords to provide services in this
-        new world, I combined all my skills to create a password manager and
-        then create an encryption service. With Cloudsky you will no longer need
-        to worry about where your passwords are or whether they will be tracked
-        or found other than by yourself.
-      </p>
-      <br />
-      <p className="text-base  text-justify">
-        <b>Why Sixcry?</b> If you read the previous paragraph, you already
-        understand where it came from. Today on the internet we can say that
-        most services are not encrypted and this will be the next generation
-        after AI. With Sixcry, all services will be able to use private
-        end-to-end encryption, making it impossible to identify intruders with
-        various encryption models. Sixcry is only available for. Cloudsky
-        service as the first use case, but it will soon be available to everyone
-        with a very small price.
+        you want to know more,{" "}
+        <a href="https://x.com/hubertryanoff" className="underline">
+          follow me on X
+        </a>
+        .
       </p>
     </div>
   );
